135b6807a8512fb58a9bcdce6e85134df6f87a71
David Craig <david.craig2@mail.dcu.ie>
2019-03-13 20:53:18 +0000

    Upload New File

---
+++ src/Diglet.js
@@ -0,0 +1,12 @@
+class Diglet {
+    constructor() {
+
+        this.genes = [];
+        this.fitness = 0;
+        
+        let this.genes[0] = Math.floor(Math.random() * thicknessArray.length);
+    }
+}
+
+
+
